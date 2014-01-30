d87d44b57c05d46640fbdc666cd2733af84749b0
Israel CA <israel.cruz@hesatecnica.com>
2014-01-30 16:50:53 -0600

    TMS TRAVEL => Added Customer in tree view

--- tms_waybill.py
+++ tms_waybill.py
@@ -738,6 +738,8 @@ class tms_travel(osv.osv):
 
     _columns = {
         'waybill_ids': fields.many2many('tms.waybill', 'tms_waybill_travel_rel', 'travel_id', 'waybill_id', 'Waybills'),
+        'default_waybill_id': fields.one2many('tms.waybill', 'travel_id', 'Waybill', readonly=True),
+        'partner_id' : fields.related('default_waybill_id', 'partner_id', type='many2one', relation='res.partner', string='Customer', store=True),
     }
 
 tms_travel()
